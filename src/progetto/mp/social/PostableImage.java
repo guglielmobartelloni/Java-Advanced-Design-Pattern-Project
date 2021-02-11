902d11e89211d5714f2b721d91c80e90f1a8ed5b
samurai <bartelloni.guglielmo@gmail.com>
2021-02-11 12:50:32 +0100

    Aggiunti test per le immagini

--- src/progetto/mp/social/PostableImage.java
+++ src/progetto/mp/social/PostableImage.java
@@ -5,6 +5,9 @@ public class PostableImage implements Postable {
 	private String imagePath;
 
 	public PostableImage(String imagePath) {
+		if(imagePath.isEmpty()) {
+			throw new IllegalArgumentException("The image path cannot be empty");
+		}
 		this.imagePath = imagePath;
 	}
 
---
+++ src/progetto/mp/social/PostableImageTest.java
@@ -0,0 +1,19 @@
+package progetto.mp.social;
+
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+public class PostableImageTest {
+
+	@Test
+	public void testGetContent() {
+		assertThat(new PostableImage("C:/path/to/image").getContent()).isEqualTo("C:/path/to/image");
+	}
+	
+	
+	@Test
+	public void testGetContentWithEmptyPath() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new PostableImage("").getContent()).withMessage("The image path cannot be empty");
+	}
+
+}
